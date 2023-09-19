a24a4819a27247d92180ebca005e1ea7a8abb2b4
LuisOw <luispolooswaldo@gmail.com>
2023-09-19 15:36:00 -0300

    Move customError function to api_error package

--- pkg/api/handler/sheet_handlers.go
+++ pkg/api/handler/sheet_handlers.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -29,13 +28,13 @@ func (sh *SheetHandler) CreateSheetHandler(c echo.Context) error {
 	sd := new(entities.SheetDto)
 	if err := c.Bind(sd); err != nil {
 		log.Println("Error binding body: ", err)
-		return customError(c, http.StatusBadRequest, api_error.DtoError)
+		return api_error.CustomError(c, http.StatusBadRequest, api_error.DtoError)
 	}
 
 	sr := repository.NewSheetRepository(sh.storage)
 	ns, err := sr.Create(sd)
 	if err != nil {
-		return customError(c, http.StatusInternalServerError, api_error.SavingError, "sheet", sd)
+		return api_error.CustomError(c, http.StatusInternalServerError, api_error.SavingError, "sheet", sd)
 	}
 	return c.JSON(http.StatusCreated, ns)
 }
@@ -44,13 +43,13 @@ func (sh *SheetHandler) GetSheetHandler(c echo.Context) error {
 	id, err := uuid.Parse(c.Param("id"))
 	if err != nil {
 		log.Println("Error parsing id as uuid", err)
-		return customError(c, http.StatusBadRequest, api_error.ParseError, "id")
+		return api_error.CustomError(c, http.StatusBadRequest, api_error.ParseError, "id")
 	}
 
 	sr := repository.NewSheetRepository(sh.storage)
 	s, err := sr.FindById(id)
 	if err != nil {
-		return customError(c, http.StatusBadRequest, api_error.NotFound, "id", id)
+		return api_error.CustomError(c, http.StatusBadRequest, api_error.NotFound, "id", id)
 	}
 
 	return c.JSON(http.StatusOK, s)
@@ -60,19 +59,19 @@ func (sh *SheetHandler) GetSheetListHandler(c echo.Context) error {
 	p, err := strconv.Atoi(c.QueryParams().Get("page"))
 	if err != nil {
 		log.Println("Error converting page from string to int", err)
-		return customError(c, http.StatusBadRequest, api_error.QueryParamError, "page")
+		return api_error.CustomError(c, http.StatusBadRequest, api_error.QueryParamError, "page")
 	}
 
 	sz, err := strconv.Atoi(c.QueryParams().Get("size"))
 	if err != nil {
 		log.Println("Error converting size from string to int", err)
-		return customError(c, http.StatusBadRequest, api_error.QueryParamError, "size")
+		return api_error.CustomError(c, http.StatusBadRequest, api_error.QueryParamError, "size")
 	}
 
 	sr := repository.NewSheetRepository(sh.storage)
 	sl, err := sr.FindAll(p, sz)
 	if err != nil {
-		return customError(c, http.StatusInternalServerError, api_error.DbError, "findAll")
+		return api_error.CustomError(c, http.StatusInternalServerError, api_error.DbError, "findAll")
 	}
 
 	return c.JSON(http.StatusOK, sl)
@@ -84,23 +83,23 @@ func (sh *SheetHandler) UpdateSheetHandler(c echo.Context) error {
 	id, err := uuid.Parse(c.Param("id"))
 	if err != nil {
 		log.Println("Error parsing id as uuid", err)
-		return customError(c, http.StatusBadRequest, api_error.ParseError, "id")
+		return api_error.CustomError(c, http.StatusBadRequest, api_error.ParseError, "id")
 	}
 
 	if err := c.Bind(os); err != nil {
 		log.Println("Error binding body: ", err)
-		return customError(c, http.StatusBadRequest, api_error.DtoError)
+		return api_error.CustomError(c, http.StatusBadRequest, api_error.DtoError)
 	}
 
 	sr := repository.NewSheetRepository(sh.storage)
 	_, err = sr.FindById(id)
 	if err != nil {
-		return customError(c, http.StatusBadRequest, api_error.NotFound, "id", id)
+		return api_error.CustomError(c, http.StatusBadRequest, api_error.NotFound, "id", id)
 	}
 
 	su, err := sr.Update(os, id)
 	if err != nil {
-		return customError(c, http.StatusInternalServerError, api_error.DbError, "update")
+		return api_error.CustomError(c, http.StatusInternalServerError, api_error.DbError, "update")
 	}
 
 	return c.JSON(http.StatusOK, su)
@@ -110,28 +109,20 @@ func (sh *SheetHandler) DeleteSheetHandler(c echo.Context) error {
 	id, err := uuid.Parse(c.Param("id"))
 	if err != nil {
 		log.Println("Error parsing id as uuid", err)
-		return customError(c, http.StatusBadRequest, api_error.ParseError, "id")
+		return api_error.CustomError(c, http.StatusBadRequest, api_error.ParseError, "id")
 	}
 
 	sr := repository.NewSheetRepository(sh.storage)
 
 	_, err = sr.FindById(id)
 	if err != nil {
-		return customError(c, http.StatusBadRequest, api_error.NotFound, "id", id)
+		return api_error.CustomError(c, http.StatusBadRequest, api_error.NotFound, "id", id)
 	}
 
 	err = sr.Delete(id)
 	if err != nil {
-		return customError(c, http.StatusInternalServerError, api_error.DbError, "delete")
+		return api_error.CustomError(c, http.StatusInternalServerError, api_error.DbError, "delete")
 	}
 
 	return c.NoContent(http.StatusNoContent)
-}
-
-func customError(c echo.Context, https int, message string, args ...any) error {
-	errs := api_error.Error{
-		Error:   http.StatusText(https),
-		Message: fmt.Sprintf(message, args...),
-	}
-	return c.JSON(https, errs)
 }
--- pkg/errors/error.go
+++ pkg/errors/error.go
@@ -1,16 +1,31 @@
 package api_error
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
 type Error struct {
 	Error   string `json:"error"`
 	Message string `json:"message"`
 }
 
 const (
-	NotFound        = "Not found with %s: %s"
-	DtoError        = "Error with Dto"
-	SavingError     = "Error saving %s with values %s"
-	ParseError      = "Error parsing %s"
-	QueryParamError = "Query param '%s' error"
-	DbError         = "Error with %s"
-  InternalServerErrorMessage = "something went wrong: %v"
+	NotFound                   = "Not found with %s: %s"
+	DtoError                   = "Error with Dto"
+	SavingError                = "Error saving %s with values %s"
+	ParseError                 = "Error parsing %s"
+	QueryParamError            = "Query param '%s' error"
+	DbError                    = "Error with %s"
+	InternalServerErrorMessage = "something went wrong: %v"
 )
+
+func CustomError(c echo.Context, https int, message string, args ...any) error {
+	errs := Error{
+		Error:   http.StatusText(https),
+		Message: fmt.Sprintf(message, args...),
+	}
+	return c.JSON(https, errs)
+}
