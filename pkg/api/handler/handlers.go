af7401455055245d05625df633e9bff93edcf3bf
Felipe Arthur <felipe.rosa.arthur27@gmail.com>
2023-09-25 13:01:14 -0300

    feat(handlers): add NPC generation handler (#16)

--- pkg/api/handler/handlers.go
+++ pkg/api/handler/handlers.go
@@ -12,9 +12,6 @@ type HealthResponse struct {
 
 func HealthHandler(c echo.Context) error {
 	r := HealthResponse{Alive: true}
-	if err := c.Bind(r); err != nil {
-		return err
-	}
 
 	return c.JSON(http.StatusOK, r)
 }
